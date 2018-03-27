043e80e953cef2a4c08d500faac181dd7fda37c6
Ronshmul <ronush.92@gmail.com>
2018-03-27 16:57:57 +0300

    download photo function

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,3 +1,5 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,30 +7,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Ronshmul on 25/03/2018.
  */
 public class Assignment1 {
 
-    public void modulate1Bits(long window) {
-        long tStart = System.currentTimeMillis();
-        while((System.currentTimeMillis()-tStart)< window*0.8){
-            sendGet();
-        }
-    }
-    public void modulate0Bits(long window) {
-        try {
-            Thread.sleep(window);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-        public static void sendGet() {
+    public static void sendGet() {
         String urlStr = "http://www.youtube.com";
         try {
             URL url = new URL(urlStr);
@@ -55,6 +40,46 @@ public class Assignment1 {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        Assignment1 ass = new Assignment1();
+        long startTime = System.currentTimeMillis();
+        ass.modulateMessage("0308089671_0308465954", 2000);
+        int[] arr = ass.asBinarySequence("0308089671_0308465954");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "," + arr[i] + ",");
+        }
+        System.out.println();
+        System.out.println(startTime);
+    }
+
+    public void modulate1Bits(long window) {
+        long tStart = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - tStart) < window * 0.8) {
+            downloadPhoto();
+        }
+    }
+
+    public void modulate0Bits(long window) {
+        try {
+            Thread.sleep(window);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void downloadPhoto() {
+        Image img = null;
+        try {
+            URL imUrl = new URL("https://vignette.wikia.nocookie.net/marvel_dc/images/9/92/Pantha_Tiny_Titans.gif/revision/latest?cb=20111020190500");
+            img = ImageIO.read(imUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void modulateMessage(String message, long window) {
         int[] binarySequence = asBinarySequence(message);
         for (int index = 0; index < binarySequence.length; index++) {
@@ -65,16 +90,14 @@ public class Assignment1 {
         }
     }
 
-    public int[] asBinarySequence(String message){
+    public int[] asBinarySequence(String message) {
         byte[] byteArr = message.getBytes();
 
         StringBuilder binary = new StringBuilder();
         int size = 0;
-        for (byte b : byteArr)
-        {
+        for (byte b : byteArr) {
             int val = b;
-            for (int i = 0; i < 8; i++)
-            {
+            for (int i = 0; i < 8; i++) {
                 binary.append((val & 128) == 0 ? 0 : 1);
                 binary.append(' ');
                 val <<= 1;
@@ -89,15 +112,6 @@ public class Assignment1 {
             i++;
         }
         return intArr;
-    }
-
-    public static void main(String[] args) {
-        Assignment1 ass= new Assignment1();
-        ass.modulateMessage("0308089671_0308465954", 5000);
-        int[] arr= ass.asBinarySequence("0308089671_0308465954");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "," +arr[i] + ",");
-        }
     }
 
 }
